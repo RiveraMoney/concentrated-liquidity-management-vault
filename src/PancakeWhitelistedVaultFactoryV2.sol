b895e04eeec4efcb3833a73f2a7716871f7a2033
Prajwal Ghate <prajwalghate18@gmail.com>
2023-06-01 14:01:35 +0530

    Added Private and Whitelisted vault factory

--- src/PancakeWhitelistedVaultFactoryV2.sol
+++ src/PancakeWhitelistedVaultFactoryV2.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.8.0;
 
 import './interfaces/IRiveraAutoCompoundingVaultFactoryV2.sol';
 import './strategies/common/interfaces/IStrategy.sol';
-import './vaults/RiveraAutoCompoundingVaultV2Public.sol';
+import './vaults/RiveraAutoCompoundingVaultV2Whitelisted.sol';
 import './PancakeStratFactoryV2.sol';
 
-contract PancakePublicVaultFactoryV2 is IRiveraAutoCompoundingVaultFactoryV2 {
+contract PancakeWhitelistedVaultFactoryV2 is IRiveraAutoCompoundingVaultFactoryV2 {
 
     address[] public allVaults;
 
@@ -15,7 +15,7 @@ contract PancakePublicVaultFactoryV2 is IRiveraAutoCompoundingVaultFactoryV2 {
     address NonfungiblePositionManager;
     address manager;
     address stratFactory;
-    VaultType immutable vaultType = VaultType.PUBLIC;
+    VaultType immutable vaultType = VaultType.WHITELISTED;
 
     constructor(address _chef, address _router, address _NonfungiblePositionManager, address _stratFactory) {
         chef = _chef;
@@ -26,7 +26,7 @@ contract PancakePublicVaultFactoryV2 is IRiveraAutoCompoundingVaultFactoryV2 {
     }
 
     function createVault(CreateVaultParams memory createVaultParams) external returns (address vaultAddress) {
-        RiveraAutoCompoundingVaultV2Public vault = new RiveraAutoCompoundingVaultV2Public(createVaultParams.asset, createVaultParams.tokenName, createVaultParams.tokenSymbol, 
+        RiveraAutoCompoundingVaultV2Whitelisted vault = new RiveraAutoCompoundingVaultV2Whitelisted(createVaultParams.asset, createVaultParams.tokenName, createVaultParams.tokenSymbol, 
         createVaultParams.approvalDelay, createVaultParams.totalTvlCap);
         vaultAddress = address(vault);
         address stratAddress = PancakeStratFactoryV2(stratFactory).createStrat(createVaultParams, vaultAddress, msg.sender, manager, router, NonfungiblePositionManager, chef);
