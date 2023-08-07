af53cd98b62ce16f963ebe3b5cb1cb932a9b30d6
HashHaran <hariharan@alumni.iitm.ac.in>
2023-08-07 10:41:14 +0530

    Making fee related params public for better transparency

--- src/strategies/common/FeeManager.sol
+++ src/strategies/common/FeeManager.sol
@@ -4,12 +4,12 @@ import "./AbstractStrategyV2.sol";
 
 contract FeeManager is AbstractStrategyV2 {
 
-    uint256 withdrawFeeDecimals;
-    uint256 withdrawFee;
+    uint256 public withdrawFeeDecimals;
+    uint256 public withdrawFee;
 
-    uint256 feeDecimals;
-    uint256 protocolFee;
-    uint256 fundManagerFee;
-    uint256 partnerFee;
-    address partner;
+    uint256 public feeDecimals;
+    uint256 public protocolFee;
+    uint256 public fundManagerFee;
+    uint256 public partnerFee;
+    address public partner;
 }
