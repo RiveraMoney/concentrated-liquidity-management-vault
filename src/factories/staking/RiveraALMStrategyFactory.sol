0d7ca65ebd9b5bb6227a3b980989076a8b84eecf
prajwalghate <prajwalghate18@gmail.com>
2023-10-06 19:06:41 +0530

    Fusionx No Farm Code

--- src/factories/staking/RiveraALMStrategyFactory.sol
+++ src/factories/staking/RiveraALMStrategyFactory.sol
@@ -1,20 +1,22 @@
 pragma solidity ^0.8.0;
 
 import './RiveraLpStakingVaultCreationStruct.sol';
-import '@rivera/strategies/staking/RiveraConcLpStaking.sol';
+import '@rivera/strategies/staking/RiveraConcNoStaking.sol';
 
 contract RiveraALMStrategyFactory {
 
-    function createStrat(RiveraVaultParams memory createVaultParams, address vaultAddress, address user, address manager, address router, address nonfungiblePositionManager, address chef, 
+    function createStrat(RiveraVaultParams memory createVaultParams, address vaultAddress, address user, address manager, address router, address nonfungiblePositionManager, /*address chef, */
     FeeParams memory feeParams) external returns (address) {
-        RiveraConcLpStaking strategy = new RiveraConcLpStaking();
-        strategy.init(RiveraLpStakingParams(createVaultParams.tickLower, createVaultParams.tickUpper, createVaultParams.stake, chef, createVaultParams.rewardToLp0AddressPath[0], createVaultParams.tickMathLib, 
-        createVaultParams.sqrtPriceMathLib, createVaultParams.liquidityMathLib, createVaultParams.safeCastLib, createVaultParams.liquidityAmountsLib, createVaultParams.fullMathLib, 
-        createVaultParams.rewardToLp0AddressPath, createVaultParams.rewardToLp0FeePath, createVaultParams.rewardToLp1AddressPath, createVaultParams.rewardToLp1FeePath, 
-        createVaultParams.rewardtoNativeFeed, createVaultParams.assettoNativeFeed,createVaultParams.pendingReward), CommonAddresses(vaultAddress, router, nonfungiblePositionManager, feeParams.withdrawFeeDecimals, 
+        RiveraConcNoStaking strategy = new RiveraConcNoStaking();
+        // strategy.init(RiveraLpStakingParams(createVaultParams.tickLower, createVaultParams.tickUpper, createVaultParams.stake, chef, createVaultParams.rewardToLp0AddressPath[0], createVaultParams.tickMathLib, 
+        // createVaultParams.sqrtPriceMathLib, createVaultParams.liquidityMathLib, createVaultParams.safeCastLib, createVaultParams.liquidityAmountsLib, createVaultParams.fullMathLib, 
+        // createVaultParams.rewardToLp0AddressPath, createVaultParams.rewardToLp0FeePath, createVaultParams.rewardToLp1AddressPath, createVaultParams.rewardToLp1FeePath, 
+        // createVaultParams.rewardtoNativeFeed, createVaultParams.assettoNativeFeed,createVaultParams.pendingReward), CommonAddresses(vaultAddress, router, nonfungiblePositionManager, feeParams.withdrawFeeDecimals, 
+        // feeParams.withdrawFee, feeParams.feeDecimals, feeParams.protocolFee, feeParams.fundManagerFee, feeParams.partnerFee, feeParams.partner,manager,user));
+
+        strategy.init(RiveraLpStakingParams(createVaultParams.tickLower, createVaultParams.tickUpper, createVaultParams.stake, createVaultParams.tickMathLib, 
+        createVaultParams.sqrtPriceMathLib, createVaultParams.liquidityMathLib, createVaultParams.safeCastLib, createVaultParams.liquidityAmountsLib, createVaultParams.fullMathLib, createVaultParams.assettoNativeFeed), CommonAddresses(vaultAddress, router, nonfungiblePositionManager, feeParams.withdrawFeeDecimals, 
         feeParams.withdrawFee, feeParams.feeDecimals, feeParams.protocolFee, feeParams.fundManagerFee, feeParams.partnerFee, feeParams.partner,manager,user));
-        // strategy.transferOwnership(user);
-        // strategy.setManager(manager);
         return address(strategy);
     }
 
